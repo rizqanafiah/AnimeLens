ef3e0a8d17619474f1b0d56c21adc571193446a3
Adrian Akhmad Firdaus <adrianakhmad03@gmail.com>
2025-06-13 00:12:37 +0800

    Update FrontEnd DetectPage.tsx Footer

--- src/pages/DetectPage.tsx
+++ src/pages/DetectPage.tsx
@@ -103,10 +103,10 @@ const DetectPage = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="flex flex-col min-h-screen bg-gray-50">
       <Navigation />
       
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="flex-grow max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="mb-8">
           <Link 
             to="/"
